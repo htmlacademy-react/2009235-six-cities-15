44cd4c38aa60a3e7488cea451ecc16cf7a887819
PolinaT <timoninapa@gmail.com>
2024-02-22 18:48:37 +0300

    Добавлена маршрутизация + Компонент логотипа

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import PageNotFoundScreen from '../../pages/page-not-found-screen/page-not-found-screen';
 
 type AppScreenProps = {
   placeCardsCount: number;
@@ -6,7 +12,19 @@ type AppScreenProps = {
 
 function App({placeCardsCount}:AppScreenProps): JSX.Element {
   return (
-    <MainScreen placeCardsCount = {placeCardsCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen placeCardsCount={placeCardsCount} />}
+        />
+
+        <Route path={AppRoute.Offer} element={<OfferScreen/>} />
+        <Route path={AppRoute.Favorites} element={<FavoritesScreen/>} />
+        <Route path={AppRoute.Login} element={<LoginScreen/>} />
+        <Route path="*" element={<PageNotFoundScreen/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,10 @@
 export const Setting = {
   PlaceCardsCount: 5,
 };
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer:id',
+}
